390d4a796af9890bc770b6d352be28b79ba9ffdf
Hugarra <75989236+Hugarra@users.noreply.github.com>
2021-05-01 14:59:43 -0300

    Landing fix

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,12 +8,12 @@ function Landing(props) {
             <VideoRow title="Buenos Aires" ></VideoRow>
             <br></br>
             <div className="container-fluid text-start">
-                <p>Copyright - Rights reserved to their respective creators</p>
-                <p>Youtube videos:</p>
-                <p><a href="https://www.youtube.com/watch?v=-TPJot7-HTs">Buenos Aires 4K - Driving Downtown - Morning Drive (J Utah)</a></p>
-                <p><a href="https://www.youtube.com/watch?v=3E4HcpHpbZs">4K DRIVE Buenos Aires ARGENTINA Jacaranda trees 4k video (4K DRIVE)</a></p>
-                <p><a href="https://www.youtube.com/watch?v=93Elhrlxk7o">4K DRIVE Buenos Aires ARGENTINA Callao Avenue 4k video VLOG (4K DRIVE)</a></p>
-                <p><a href="https://www.youtube.com/watch?v=DKHdsB5_oyc">[4K] Buenos Aires Walk - Catalinas Norte - Barrio de Retiro / Buenos Aires- Argentina (BAIRES WALKER)</a></p>
+                <p className="m-1">Copyright - Rights reserved to their respective creators</p>
+                <p className="m-1">Youtube videos:</p>
+                <p className="m-1"><a href="https://www.youtube.com/watch?v=-TPJot7-HTs">Buenos Aires 4K - Driving Downtown - Morning Drive (J Utah)</a></p>
+                <p className="m-1"><a href="https://www.youtube.com/watch?v=3E4HcpHpbZs">4K DRIVE Buenos Aires ARGENTINA Jacaranda trees 4k video (4K DRIVE)</a></p>
+                <p className="m-1"><a href="https://www.youtube.com/watch?v=93Elhrlxk7o">4K DRIVE Buenos Aires ARGENTINA Callao Avenue 4k video VLOG (4K DRIVE)</a></p>
+                <p className="m-1"><a href="https://www.youtube.com/watch?v=DKHdsB5_oyc">[4K] Buenos Aires Walk - Catalinas Norte - Barrio de Retiro / Buenos Aires- Argentina (BAIRES WALKER)</a></p>
             </div>
         </div>
     );
